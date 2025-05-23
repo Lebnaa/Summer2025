2b8e09764384953dc71cedfe1770285460007495
Lebnaa <127699913+Lebnaa@users.noreply.github.com>
2025-05-23 16:59:40 -0400

    add

---
+++ CCP555NSA/Labs/Lab02/fragments--ui/src/app.js
@@ -0,0 +1,38 @@
+// src/app.js
+
+import { signIn, getUser } from './auth';
+// modifications to src/app.js
+import { getUserFragments } from './api';
+
+
+async function init() {
+  // Get our UI elements
+  const userSection = document.querySelector('#user');
+  const loginBtn = document.querySelector('#login');
+  
+
+  // Wire up event handlers to deal with login and logout.
+  loginBtn.onclick = () => {
+    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+    signIn();
+  };
+
+  // See if we're signed in (i.e., we'll have a `user` object)
+  const user = await getUser();
+  if (!user) {
+    return;
+  }
+  const userFragments = await getUserFragments(user);
+
+  // Update the UI to welcome the user
+  userSection.hidden = false;
+
+  // Show the user's username
+  userSection.querySelector('.username').innerText = user.username;
+
+  // Disable the Login button
+  loginBtn.disabled = true;
+}
+
+// Wait for the DOM to be ready, then start the app
+addEventListener('DOMContentLoaded', init);
